81a07ebdc7489ed6b7fb4af6ed675de56f74c759
Kamil Durkiewicz <kamil@durkiewicz.pl>
2023-06-10 07:17:37 +0200

    The color scheme was changed to vscode.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,16 +1,20 @@
 local M = {
-  "folke/tokyonight.nvim",
-  commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
+  "Mofiqul/vscode.nvim",
+  commit = "3ae7e188f0b58b29bc1b5d3698e47ce54f51caf9",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-M.name = "tokyonight-night"
+M.name = "vscode"
 function M.config()
-  local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
-  if not status_ok then
-    return
-  end
+    require('vscode').setup({
+        style = 'light',
+        italic_comments = true,
+    })
+    local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
+    if not status_ok then
+        return
+    end
 end
 
 return M
